78a88f540a63adbf281b1800af6f7fb62f660fdd
Liam Morales Melo <liammoralesmelo@MacBook-Air-de-Liam-4.local>
2017-06-22 12:55:48 -0500

    last homework's added

---
+++ Factorial.py
@@ -0,0 +1,6 @@
+y=1
+x=int (input("ingresa un numero a evaluar= "))
+for i in range(x,1,-1):
+	y=y*i
+
+print(y)
---
+++ Funciones.py
@@ -0,0 +1,16 @@
+def trianguloPerimetro(x,y,w):
+	z=(x+y+w)
+	print(z)
+def cuadradoPerimetro(x):
+	z=x*4
+	print(z)
+def circuloPerimetro(r):
+	z=(r*2)*3.1416
+	print(z)
+
+Circulo=circuloPerimetro(4)
+Triangulo=trianguloPerimetro(3,2,2)
+Cuadrado=cuadradoPerimetro(5)
+
+
+	
